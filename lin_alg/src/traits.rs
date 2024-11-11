1d71f7aaa90d590d30c12af65e8c63a203fd6624
Jimmy Ostler <jtostler1@gmail.com>
2024-11-11 12:43:33 -0800

    made traits better

--- lin_alg/src/traits.rs
+++ lin_alg/src/traits.rs
@@ -13,29 +13,11 @@ pub trait RingElement:
 /// If Default is not the additive identity, this library will give odd results
 /// Keep this in mind when using user-defined algebraic types, as this trait is auto implemented
 /// for any type that implements all of the contained traits
-pub trait FieldElement:
-    Add<Output = Self>
-    + Sub<Output = Self>
-    + Mul<Output = Self>
-    + Div<Output = Self>
-    + Clone
-    + Copy
-    + Default
-{
-}
+pub trait FieldElement: RingElement + Div<Output = Self> {}
 
 impl<T> RingElement for T where
     T: Add<Output = Self> + Sub<Output = Self> + Mul<Output = Self> + Clone + Copy + Default
 {
 }
 
-impl<T> FieldElement for T where
-    T: Add<Output = Self>
-        + Sub<Output = Self>
-        + Mul<Output = Self>
-        + Div<Output = Self>
-        + Clone
-        + Copy
-        + Default
-{
-}
+impl<T> FieldElement for T where T: RingElement + Div<Output = Self> {}
